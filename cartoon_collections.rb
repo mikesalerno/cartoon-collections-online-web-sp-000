79bb1dc758e8798973bf16f8798bf2dca55898bc
Michael <wtfdic@hotmail.com>
2020-11-27 02:59:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(long_calls)# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  long_calls.any? do |lc|
+    lc.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
